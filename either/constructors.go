cccab19d5cb74709639612850f19cb054b8dd122
Sebastian Gamboa <me@sagmor.com>
2022-08-19 16:27:37 -0700

    Fix nil call

--- either/constructors.go
+++ either/constructors.go
@@ -5,10 +5,10 @@ import "github.com/sagmor/fun"
 
 // Left builds an Either with it's Left value set
 func Left[R, L any](val L) fun.Either[L,R] {
-	return fun.NewEither(true, val, fun.Empty[R]())
+	return fun.NewEither(true, val, fun.Nil[R]())
 }
 
 // Right builds an Either with it's Right value set
 func Right[L, R any](val R) fun.Either[L,R] {
-	return fun.NewEither(false, fun.Empty[L](), val)
+	return fun.NewEither(false, fun.Nil[L](), val)
 }
