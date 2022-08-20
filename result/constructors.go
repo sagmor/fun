023ca177b9d99314c5dad4dd497f5a795176297d
Sebastian Gamboa <me@sagmor.com>
2022-08-20 06:09:25 +0000

    Add Result monad

---
+++ result/constructors.go
@@ -0,0 +1,14 @@
+// Package result implements functions for working with the Result type
+package result
+
+import "github.com/sagmor/fun"
+
+// Success returns a successful result.
+func Success[T any](val T) fun.Result[T] {
+	return fun.NewResult(val, nil)
+}
+
+// Failure returns a failed result.
+func Failure[T any](err error) fun.Result[T] {
+	return fun.NewResult(fun.Nil[T](), err)
+}
