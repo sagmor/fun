fbb00316231604a2b7ac1ab77f3437f86296abfb
Sebastian Gamboa <me@sagmor.com>
2022-08-19 23:14:11 -0700

    Either updates (#8)
    Adding a few extra functions and tests for Either

--- tests/either_test.go
+++ tests/either_test.go
@@ -14,6 +14,14 @@ func TestLeft(t *testing.T) {
 	assert.True(t, e.IsLeft())
 	assert.False(t, e.IsRight())
 	assert.Equal(t, 3, e.Left())
+	assert.Equal(t, 3, e.RequireLeft())
+
+	v, _ := e.Get()
+	assert.Equal(t, 3, v)
+
+	assert.Panics(t, func() {
+		e.RequireRight()
+	})
 }
 
 func TestRight(t *testing.T) {
@@ -22,4 +30,12 @@ func TestRight(t *testing.T) {
 	assert.True(t, e.IsRight())
 	assert.False(t, e.IsLeft())
 	assert.Equal(t, "hello", e.Right())
+	assert.Equal(t, "hello", e.RequireRight())
+
+	_, v := e.Get()
+	assert.Equal(t, "hello", v)
+
+	assert.Panics(t, func() {
+		e.RequireLeft()
+	})
 }
