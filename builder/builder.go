df9bfa69a0b7f2bbd19f2a950252bf30f68190ae
Sebastian Gamboa <me@sagmor.com>
2022-12-07 17:18:16 -0800

    Creating more builder chain helper functions (#32)
    To allow for more complex builder chains we are adding:
    * `builder.FromFunction`
    * `builder.FromResult`

--- builder/builder.go
+++ builder/builder.go
@@ -67,3 +67,26 @@ func WithOptions[T any](options []Option[T]) Option[T] {
 		return nil
 	}
 }
+
+// WithError just fails a builder chain with the specified error.
+func WithError[T any](err error) Option[T] {
+	return func(*T) error {
+		return err
+	}
+}
+
+// FromFunction allows to derive options from an arbitrary function.
+func FromFunction[T any](function func() Option[T]) Option[T] {
+	return func(t *T) error {
+		return function()(t)
+	}
+}
+
+// FromResult allows to derive options from a fun.Result value.
+func FromResult[R, T any](r fun.Result[R], then func(R) Option[T]) Option[T] {
+	if r.IsFailure() {
+		return WithError[T](r.Error())
+	}
+
+	return then(r.RequireValue())
+}
